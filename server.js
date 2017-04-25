d9f122e57883bdebaa47eb46a791d4634befb103
Astro <astro@spaceboyz.net>
2017-04-26 00:30:16 +0200

    100% repurpose

--- server.js
+++ server.js
@@ -1,302 +1,66 @@
-var XMPP = require('node-xmpp');
-var Connect = require('connect');
-var Formidable = require('formidable');
-var Crypto = require('crypto');
-var hqswitch = require('./hqswitch');
-var fs = require('fs');
+const Crypto = require('crypto');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const XMPPClient = require('./xmpp_client');
 
+const SPACEAPI_URL = "http://www.hq.c3d2.de:3000/spaceapi.json";
+
+
+process.on('SIGTERM', function() {
+    process.exit(0);
+});
 
 if (process.argv.length != 5) {
     console.error("Parameters: <my-jid> <my-password> <full-muc-jid>");
     process.exit(1);
 }
-var jid = process.argv[2],
-pass = process.argv[3],
-muc_jid = process.argv[4],
-muc_room_jid = (new XMPP.JID(muc_jid)).bare().toString();
+const jid = process.argv[2],
+      pass = process.argv[3],
+      muc_jid = process.argv[4];
 
-/** XMPP stuff **/
+const cl = new XMPPClient(jid, pass);
+cl.joinRoom(muc_jid);
 
-var NS_MUC = 'http://jabber.org/protocol/muc';
-var NS_VCARD = 'vcard-temp';
-var NS_VCARD_UPDATE = 'vcard-temp:x:update';
-var client = null;
-var clientOnline = false;
-function setupClient() {
-    clientOnline = false;
-    client = new XMPP.Client({ jid: jid,
-			       password: pass,
-			       reconnect: true
-			     });
-    client.on('online', function() {
-	console.log("online");
-	clientOnline = true;
-        setFromSwitch(hqswitch.state);
-    });
-    client.on('error', function(e) {
-console.log("client error", e.message || e);
-//process.nextTick(setupClient);
-    });
-    client.on('end', function() {
-console.log("client end");
-	if (clientOnline) {
-	    // we were online, we can retry
-	    clientOnline = true;
-	    process.nextTick(setupClient);
-	} else {
-	    // we didn't get beyond auth, die
-	    process.exit(1);
-	}
-    });
-    client.on('error', function(e) {
-console.log(e);
-           process.exit(1);
-    });
-    client.on('rawStanza', function(stanza) {
-	console.log("<<", stanza.toString());
-    });
-    // Auto-subscribe
-    client.on('stanza', function(stanza) {
-	if (stanza.attrs.type == 'subscribe')
-	    client.send(new XMPP.Element('presence',
-					 { to: stanza.attrs.from,
-					   type: 'subscribed'
-					 }));
-    });
+
+function spaceAPI() {
+    return fetch(SPACEAPI_URL)
+        .then(res => res.json());
 }
 
-setupClient();
+cl.on('muc:message', (muc, nick, text) => {
+    var m;
 
-function relayMessage(text) {
-    if (clientOnline)
-	client.send(new XMPP.Element('message',
-				     { to: muc_room_jid,
-				       type: 'groupchat'
-				     }).
-		    c('body').t(text)
-		   );
-    else
-	throw 'Not online yet';
-}
+    if (text == "+hq status") {
+        spaceAPI().then(json => {
+            if (json.state &&
+                json.state.hasOwnProperty('open') &&
+                json.state.hasOwnProperty('message')) {
 
-function setAvatar(photo, type) {
-    var photo64 = photo.toString('base64');
-    var photo_sha1 = Crypto.createHash('sha1').
-	update(photo).digest('hex');
-
-    client.send(new XMPP.Element('iq',
-				 { type: 'set' }).
-		c('vCard', { xmlns: NS_VCARD }).
-		c('NICKNAME').t('MUC Bot').up().
-		c('PHOTO').
-		c('BINVAL').t(photo64).up().
-		c('TYPE').t(type)
-	       );
-    return function(presence) {
-	return presence.
-	    c('x', { xmlns: NS_VCARD_UPDATE }).
-	    c('photo').t(photo_sha1).up().up();
-    };
-}
-
-var AVATAR_IMGS = {
-    '0': fs.readFileSync("hq_status/hq_is_off.ink.png"),
-    '1': fs.readFileSync("hq_status/hq_is_on.ink.png"),
-    '2': fs.readFileSync("hq_status/hq_is_full.ink.png")
-};
-
-var oldState;
-function setFromSwitch(state) {
-if (!clientOnline) return;
-
-    if (state === oldState)
-        return;
-console.log("setFromSwitch", oldState, state);
-    oldState = state;
-
-    if (clientOnline) {
-/* check if client is still "online"*/
-	var text, status;
-	switch(state) {
-	    case 0:
-		text = "HQ is off.";
-		status = 'away';
-		break;
-	    case 1:
-		text = "HQ is on.";
-		status = '';
-		break;
-	    case 2:
-		text = "HQ is full.";
-		status = 'chat';
-		break;
-	    default:
-		text = "HQ is unknown?";
-		status = 'xa';
-		break;
-	}
-	client.send(new XMPP.Element('message',
-				     { type: 'groupchat',
-				       to: muc_room_jid
-				     }).
-		    c('body').t(text)
-		   );
-	var presence = new XMPP.Element('presence').
-		c('status').t(text).up().
-		c('show').t(status).up();
-	var applyAvatarHash = setAvatar(AVATAR_IMGS[state], "image/png");
-	presence = applyAvatarHash(presence);
-console.log("presence", presence.root().toString());
-	client.send(presence);
-
-console.log("join muc", muc_jid);
-	presence = new XMPP.Element('presence', { to: muc_jid }).
-		c('status').t(text).up().
-		c('show').t(status).up().
-		c('x', { xmlns: NS_MUC }).up();
-	client.send(applyAvatarHash(presence));
+                const open = !! json.state.hasOwnProperty('open');
+                cl.sendRoomMessage(muc, `${nick}: [${open ? "OPEN" : "CLOSED"}] ${json.state.message}`);
+            }
+        });
+    } else if (text == "+hq sensors") {
+        spaceAPI().then(json => {
+            const categories = Object.keys(json.sensors || {});
+            cl.sendRoomMessage(muc, `${nick}: +hq sensors <${categories.join(" | ")}>`);
+        });
+    } else if ((m = text.match(/^\+hq sensors (.+)$/))) {
+        const category = m[1];
+        spaceAPI().then(json => {
+            const readings = json.sensors[category];
+            if (!readings) {
+                cl.sendRoomMessage(muc, `${nick}: No such sensors`);
+            } else {
+                var text = `${nick}, ${category} sensors:`;
+                for(const { name, value, unit, location } of readings) {
+                    text += `\n${name}: ${value} ${unit}`;
+                    if (location) {
+                        text += ` (${location})`;
+                    }
+                }
+                cl.sendRoomMessage(muc, text);
+            }
+        });
     }
-/*
-  else {
-    process.nextTick(client.reconnect);
-  }
-*/
-}
-var debounceSwitch;
-var DEBOUNCE_TIME = 1000;
-hqswitch.on('switch', function(state) {
-    if (debounceSwitch) {
-	clearTimeout(debounceSwitch);
-	debounceSwitch = null;
-    }
-    debounceSwitch = setTimeout(function() {
-	setFromSwitch(state);
-    }, DEBOUNCE_TIME);
-});
-
-/** Web stuff **/
-
-function handlePartInBuffer(part) {
-    var form = this;
-
-    var bufs = [], bufsLen = 0;
-    part.on('data', function(data) {
-	bufs.push(data);
-	bufsLen += data.length;
-    });
-    part.on('end', function() {
-	var data = new Buffer(bufsLen)
-	var offset = 0;
-	for(var i = 0; i < bufs.length; i++) {
-	    bufs[i].copy(data, offset, 0);
-	    offset += bufs[i].length;
-	}
-	
-	form.emit('file', part.name,
-		  { filename: part.filename,
-		    mime: part.mime,
-		    data: data
-		  });
-    });
-}
-
-var lastSwitchChange = Math.floor(new Date().getTime() / 1000);
-hqswitch.on('switch', function() {
-    lastSwitchChange = Math.floor(new Date().getTime() / 1000);
-});
-
-Connect.createServer(
-    Connect.logger(),
-    Connect.bodyParser(),
-    Connect.router(function(app) {
-	// curl --data-urlencode "text=Hello, World" http://localhost:4000/msg
-	app.post('/msg', function(req, res) {
-	    var text;
-	    if (req.body && (text = req.body.text)) {
-		relayMessage(text);
-		res.writeHead(200, {});
-		res.end('Sent');
-	    } else {
-		res.writeHead(400, {});
-		res.end('Only application/x-www-form-urlencoded and application/json are permitted. ' +
-			'There must be a `text\' field');
-	    }
-	});
-
-	// Space API
-	app.get('/spaceapi.json', function(req, res) {
-	    function errback(e) {
-		res.writeHead(500, { "Content-Type": "text/plain" });
-		res.write(e.stack || e.message || e);
-		res.end();
-	    }
-
-	    fs.readFile('spaceapi.json', 'utf8', function(err, data) {
-		if (err)
-		    return errback(err);
-
-		var json;
-		try {
-		    json = JSON.parse(data);
-		    switch(hqswitch.state) {
-			case 0:
-			    json.state.open = false;
-			    json.state.message = "HQ is off.";
-			    break;
-			case 1:
-			    json.state.open = true;
-			    json.state.message = "HQ is on.";
-			    break;
-			case 2:
-			    json.state.open = true;
-			    json.state.message = "HQ is full.";
-			    break;
-		    }
-		    json.lastchange = json.state.lastchange = lastSwitchChange;
-		    json.open = json.state.open;
-		    json.status = json.state.message;
-		} catch (e) {
-		    return errback(e);
-		}
-		var temp;
-		try {
-		    temp = parseInt(fs.readFileSync("/sys/class/thermal/thermal_zone0/temp"), 10) / 1000;
-		} catch (e) { }
-		if (temp)
-		    json.sensors = {
-			temperature: [
-			    { name: "beere.hq.c3d2.de",
-			      value: temp,
-			      unit: "°C",
-			      location: "Raspberry Pi neben der Tür"
-			    }
-			]
-		    };
-
-		res.writeHead(200, { "Content-Type": "application/json",
-				     "Access-Control-Allow-Origin": "*",
-				     "Cache-Control": "no-cache"
-				   });
-		res.write(JSON.stringify(json));
-		res.end();
-	    });
-	});
-
-	app.get('/status.png', function(req, res) {
-	    var location;
-	    if (hqswitch.state === 2)
-		location = "https://raw.github.com/astro/tigger/master/hq_status/hq_is_full.ink.png";
-	    else if (hqswitch.state === 1)
-		location = "https://raw.github.com/astro/tigger/master/hq_status/hq_is_on.ink.png";
-	    else
-		location = "https://raw.github.com/astro/tigger/master/hq_status/hq_is_off.ink.png";
-	    res.writeHead(302, { "Location": location });
-	    res.end();
-	});
-    }),
-    Connect.errorHandler({ dumpExceptions: true, showStack: true })
-).listen(4000, '::');
-
-process.on('SIGTERM', function() {
-    process.exit(0);
 });
---
+++ xmpp_client.js
@@ -0,0 +1,101 @@
+const XMPP = require('node-xmpp-client');
+
+const NS_MUC = 'http://jabber.org/protocol/muc';
+const NS_DELAY = 'urn:xmpp:delay';
+const NS_X_DELAY = 'jabber:x:delay';
+
+module.exports = class XMPPClient extends process.EventEmitter {
+    constructor(jid, pass) {
+        super();
+
+        this.jid = new XMPP.JID(jid);
+        this.pass = pass;
+        this.online = false;
+        this.presence = {};
+        this.rooms = {};
+        this._setup();
+    }
+
+    _setup() {
+        this.client = new XMPP.Client({
+            jid: this.jid,
+	    password: this.pass,
+	    reconnect: true,
+	});
+        this.client.on('online', () => {
+            this.online = true;
+            this.sendPresence();
+            for(var jid in this.rooms) {
+                this.joinRoom(jid);
+            }
+        });
+        this.client.on('end', () => {
+            this.online = false;
+        });
+        this.client.on('stanza', stanza => {
+            // console.log('<< ' + stanza.toString());
+
+            if (stanza.is('message') &&
+                stanza.attrs.type === 'groupchat') {
+
+                const from = new XMPP.JID(stanza.attrs.from);
+                const mucJid = from.bare().toString();
+                const fromNick = from.resource;
+                const body = stanza.getChildText('body');
+                console.log(`[${mucJid}] <${fromNick}> ${body || ""}`);
+                if (body && !stanza.getChild('delay', NS_DELAY) && !stanza.getChild('x', NS_X_DELAY)) {
+                    this.emit('muc:message', mucJid, fromNick, body);
+                }
+            }
+        });
+    }
+
+    sendPresence() {
+        this.client.send(this.generatePresence());
+    }
+    
+    generatePresence() {
+	const presence = new XMPP.Element('presence');
+        if (this.presence.status) {
+	    c('status').t(this.presence.status);
+        }
+        if (this.presence.show) {
+	    c('show').t(this.presence.show);
+        }
+        return presence;
+    }
+
+    joinRoom(jid) {
+        jid = new XMPP.JID(jid);
+        const mucJid = jid.bare().toString();
+        if (!this.rooms[mucJid]) {
+            this.rooms[mucJid] = {
+                nick: jid.resource,
+            };
+        }
+        const room = this.rooms[mucJid];
+
+        if (this.online) {
+            const presence = this.generatePresence();
+            presence.attrs.to = `${mucJid}/${room.nick}`;
+            presence.c('x', { xmlns: NS_MUC });
+            this.client.send(presence);
+        }
+    }
+
+    sendRoomMessage(jid, text) {
+        jid = new XMPP.JID(jid);
+        const mucJid = jid.bare().toString();
+
+        this.client.send(
+            new XMPP.Element('message', {
+                to: mucJid,
+                type: 'groupchat',
+            })
+                .c('body')
+                .t(text)
+                .root()
+        );
+        console.log(`[${mucJid}] >> ${text}`);
+    }
+}
