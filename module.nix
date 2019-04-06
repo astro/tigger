dd6d37b6870f4caa8c62317565ce2e184ab58815
Astro <astro@spaceboyz.net>
2019-04-07 00:38:04 +0200

    add module.nix for NixOS

---
+++ module.nix
@@ -0,0 +1,51 @@
+{ lib, pkgs, config, ... }:
+
+with lib;
+{
+  options.services.tigger = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable the bot";
+    };
+    jid = mkOption {
+      type = types.string;
+      description = "Jabber-ID";
+    };
+    password = mkOption {
+      type = types.string;
+      description = "Jabber password";
+    };
+    muc = mkOption {
+      type = types.string;
+      description = "MUC Jabber-ID";
+    };
+  };
+
+  config.systemd.services.tigger =
+    let
+      cfg = config.services.tigger;
+      tigger = pkgs.callPackage ./default.nix {};
+    in {
+      description = "MUC bot";
+      wantedBy = [ "multi-user.target" ];
+      after    = [ "network.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${tigger}/bin/tigger \
+            ${with cfg; escapeShellArgs [jid password muc]}
+        '';
+        Restart = "always";
+        RestartSec = "10min";
+
+        DynamicUser = true;
+        NoNewPrivileges = true;
+        LimitNPROC = 16;
+        LimitNOFILE = 1024;
+        CPUWeight = 5;
+        MemoryMax = "512M";
+        ProtectSystem = "full";
+     };
+  };
+}
