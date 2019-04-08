228670d138c53dcd09a235e8447bc8cafa320b56
Astro <astro@spaceboyz.net>
2019-04-08 02:35:50 +0200

    module.nix: check enable flag

--- module.nix
+++ module.nix
@@ -22,30 +22,32 @@ with lib;
     };
   };
 
-  config.systemd.services.tigger =
+  config =
     let
       cfg = config.services.tigger;
       tigger = pkgs.callPackage ./default.nix {};
-    in {
-      description = "MUC bot";
-      wantedBy = [ "multi-user.target" ];
-      after    = [ "network.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = ''
-          ${tigger}/bin/tigger \
-            ${with cfg; escapeShellArgs [jid password muc]}
-        '';
-        Restart = "always";
-        RestartSec = "10min";
+    in mkIf cfg.enable {
+      systemd.services.tigger = {
+        description = "MUC bot";
+        wantedBy = [ "multi-user.target" ];
+        after    = [ "network.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = ''
+            ${tigger}/bin/tigger \
+              ${with cfg; escapeShellArgs [jid password muc]}
+          '';
+          Restart = "always";
+          RestartSec = "10min";
 
-        DynamicUser = true;
-        NoNewPrivileges = true;
-        LimitNPROC = 16;
-        LimitNOFILE = 1024;
-        CPUWeight = 5;
-        MemoryMax = "512M";
-        ProtectSystem = "full";
+          DynamicUser = true;
+          NoNewPrivileges = true;
+          LimitNPROC = 16;
+          LimitNOFILE = 1024;
+          CPUWeight = 5;
+          MemoryMax = "512M";
+          ProtectSystem = "full";
+       };
      };
   };
 }
